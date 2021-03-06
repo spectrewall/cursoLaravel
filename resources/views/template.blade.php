92a57e490635e1f3a74c4ce92b68c577b8fd8d6a
Wallace Carvalho <wallace.carvalho789@gmail.com>
2021-03-06 20:49:47 -0300

    View template and Controller

---
+++ resources/views/template.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>
+        @yield('title')
+    </title>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
