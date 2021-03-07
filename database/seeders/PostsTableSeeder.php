f7c4193e26fc087c177b3e98b3aac021b848f6fb
Wallace Carvalho <wallace.carvalho789@gmail.com>
2021-03-07 17:26:30 -0300

    Factory and Seeder

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Model;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::truncate();
+        Post::factory()->count(10)->create();
+    }
+}
