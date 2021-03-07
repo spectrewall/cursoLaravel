7ba708d74e4c88d556d6a0a23916dc2795e39378
Wallace Carvalho <wallace.carvalho789@gmail.com>
2021-03-07 18:54:31 -0300

    implementado comentarios nos posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,6 +9,14 @@ BLOG
 @foreach($posts as $post)
 <h2>{{$post->title}}</h2>
 <p>{{$post->content}}</p>
+
+<h3>Comments</h3>
+@foreach($post->comments as $comment)
+<b>{{$comment->name}}</b><br>
+{{$comment->comment}}<br>
+<br>
+@endforeach
+
 <hr>
 @endforeach
 @stop
